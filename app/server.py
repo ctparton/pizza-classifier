d146c252e3f29a723d2cdbaf30905878b17ec48c
ctparton <52533842+ctparton@users.noreply.github.com>
2020-11-08 00:36:03 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://mega.nz/file/IZ0lhCTY#4_07x44j_Hdf68jWF1Pmjavw8GYkHewIj-4kmsRDPDs'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['romana', ' viennese', 'capricciosa', 'quattro formaggi', 'bianca', 'margherita', 'marinara']
 path = Path(__file__).parent
 
 app = Starlette()
